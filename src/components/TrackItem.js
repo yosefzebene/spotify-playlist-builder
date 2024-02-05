0c91edbd06edac980a3710d21667d22cbc7c7ea4
yosef-ZzZ <yosef.zebene1@gmail.com>
2024-02-04 19:58:17 -0600

    Added preview player

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -10,12 +10,19 @@ const TrackItem = ({ data, onAddTrackClick, onRemoveTrackClick }) => {
                 src={data.track ? data.track.album.images[0].url : data.album.images[0].url}
              />
             <Card.Body>
-                <Card.Title>{data.track ? data.track.name : data.name}</Card.Title>
+                <Card.Title 
+                    onClick={() => window.open(data.track ? data.track.external_urls.spotify :  data.external_urls.spotify, '_blank')}
+                >
+                    {data.track ? data.track.name : data.name}
+                </Card.Title>
                 <Card.Subtitle>
                 {data.track ? 
                     data.track.artists.map(artist => artist.name + " ") :
                     data.artists.map(artist => artist.name + " ")}
                 </Card.Subtitle>
+                { data.preview_url &&
+                    <audio className='preview-player' controls controlsList="nodownload" src={data.preview_url}></audio>
+                }
             </Card.Body>
             {data.track ?
                 <Button className='remove-button' onClick={() => onRemoveTrackClick(data.track.uri)}>Remove</Button>
